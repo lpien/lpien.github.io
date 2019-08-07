5356dce924703e06890450c230cbbaaf582036c0
Lauren <lpien@u.rochester.edu>
2019-08-06 17:07:00 -0700

    css

--- contact.php
+++ contact.php
@@ -1,5 +1,16 @@
 <?php
 if($_POST["message"]) {
+
+	$email_to = "lpien@u.rochester.edu";
+	$email_subject = "CONTACT PAGE FROM WEBSITE - ";
+
+	function died($error){
+		echo "The following was incorrect: ";
+		echo $error;
+		die();
+	}
+
+	if(!isset ($_POST['']))
     mail("your@email.address", "Form to email message", $_POST["message"], "From: an@email.address");
 }
 ?>

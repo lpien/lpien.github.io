a707645b611d43838cbda2ed9005f739fccfa8a9
Lauren <lpien@u.rochester.edu>
2019-08-25 18:43:09 -0700

    contact form

--- contact.php
+++ contact.php
@@ -1,5 +1,16 @@
 <?php
-if($_POST["message"]) {
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$formContent = "From: $name, $email \n $message";
+$recipient = "lpien@u.rochester.edu";
+
+mail($recipient, $subject, $formContent) or die ("error");
+echo "Thank You!";
+
+/*if($_POST["message"]) {
 
 	$email_to = "lpien@u.rochester.edu";
 	$email_subject = "CONTACT PAGE FROM WEBSITE - ";
@@ -12,5 +23,5 @@ if($_POST["message"]) {
 
 	if(!isset ($_POST['']))
     mail("your@email.address", "Form to email message", $_POST["message"], "From: an@email.address");
-}
+}*/
 ?>
